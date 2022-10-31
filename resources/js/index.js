64615f0a1559fd0b8f664ddfc22091fc88ed8fd3
LukaB15 <lukabrabant15@gmail.com>
2022-10-31 20:44:33 +0100

    video

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,3 +1,31 @@
+//video
+function toggle(){
+  var video1 = document.querySelector(".video1");
+  var video = document.querySelector("video");
+  video1.classList.toggle("active");
+  video.pause();
+  video.currentTime = 0; 
+}
+
+//slider
+// Instantiate the Bootstrap carousel
+$('.multi-item-carousel').carousel({
+  interval: false
+  });
+  // for every slide in carousel, copy the next slide's item in the slide.
+  // Do the same for the next, next item.
+  $('.multi-item-carousel .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+  next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+  if (next.next().length>0) {
+  next.next().children(':first-child').clone().appendTo($(this));
+  } else {
+  $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+  }
+  });
 
 //FAQ
 var acc = document.getElementsByClassName("accordion");
