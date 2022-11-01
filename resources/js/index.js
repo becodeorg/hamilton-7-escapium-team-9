227da4f670be182b25bb8503c8e28294ccaf9c6e
LukaB15 <lukabrabant15@gmail.com>
2022-11-01 15:19:17 +0100

    contactpage

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,3 +1,4 @@
+//ABOUR US
 //video
 function toggle(){
   var video1 = document.querySelector(".video1");
@@ -56,3 +57,230 @@ if (panel.style.maxHeight){
 });
 }
 
+//CONTACT
+document.getElementById("firstname").addEventListener("keyup", check);
+document.getElementById("email").addEventListener("keyup", check);
+document.getElementById("msg").addEventListener("keyup", check);
+
+function check(){
+  var firstname = document.getElementById("firstname").value;
+  var email = document.getElementById("email").value;
+  var msg = document.getElementById("msg").value;
+
+  if(firstname.length === 0 || email.length === 0 || msg.length === 0){
+      document.getElementById("firstname").style.borderBottom = "solid #F60B0E";
+      document.getElementById("email").style.borderBottom  = "solid #F60B0E";
+      document.getElementById("msg").style.borderBottom  = "solid #F60B0E";
+      document.getElementById("Success").style.visibility ="hidden";
+  }
+  else{
+    document.getElementById("firstname").style.borderBottom = "solid #717171";
+    document.getElementById("email").style.borderBottom  = "solid #717171";
+    document.getElementById("msg").style.borderBottom  = "solid #717171";
+    document.getElementById("Success").style.visibility ="visible";
+  }
+}
+
+// var test= {lat: 40.779897, lng: -73.968565};
+//MAP
+function initMap() {
+  var test= {lat: 40.779897, lng: -73.968565};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 9,
+    center: test,
+    styles: [
+      {
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#212121"
+          }
+        ]
+      },
+      {
+        "elementType": "labels.icon",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#757575"
+          }
+        ]
+      },
+      {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+          {
+            "color": "#212121"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#757575"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative.country",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#9e9e9e"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative.land_parcel",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative.locality",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#bdbdbd"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#757575"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#181818"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#616161"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+          {
+            "color": "#1b1b1b"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {
+            "color": "#2c2c2c"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#8a8a8a"
+          }
+        ]
+      },
+      {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#373737"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#3c3c3c"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway.controlled_access",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#4e4e4e"
+          }
+        ]
+      },
+      {
+        "featureType": "road.local",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#616161"
+          }
+        ]
+      },
+      {
+        "featureType": "transit",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#757575"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#000000"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#3d3d3d"
+          }
+        ],
+      },
+    ],
+  }
+  );
+
+  var marker = new google.maps.Marker({
+    position: test,
+    map: map,
+    icon: '../resources/images/gmap-logo.png',
+  });
+  
+}
