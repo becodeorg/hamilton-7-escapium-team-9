b078e41d301618d6d00dcebb9e2c8b5867ee257d
LukaB15 <lukabrabant15@gmail.com>
2022-11-01 11:27:26 +0100

    about-us__end

--- resources/js/index.js
+++ resources/js/index.js
@@ -8,25 +8,32 @@ function toggle(){
 }
 
 //slider
-// Instantiate the Bootstrap carousel
-$('.multi-item-carousel').carousel({
-  interval: false
-  });
-  // for every slide in carousel, copy the next slide's item in the slide.
-  // Do the same for the next, next item.
-  $('.multi-item-carousel .item').each(function(){
-  var next = $(this).next();
-  if (!next.length) {
-  next = $(this).siblings(':first');
-  }
-  next.children(':first-child').clone().appendTo($(this));
-  if (next.next().length>0) {
-  next.next().children(':first-child').clone().appendTo($(this));
-  } else {
-  $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-  }
-  });
+var slideIndex = 1;
+showSlides(slideIndex);
 
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("WhatPeopleSay__bloc");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
 //FAQ
 var acc = document.getElementsByClassName("accordion");
 var i;
