d08fe466a5905548145c1423030e014bf8900473
LukaB15 <lukabrabant15@gmail.com>
2022-11-04 14:05:01 +0100

    pictur

--- resources/js/index.js
+++ resources/js/index.js
@@ -33,7 +33,7 @@ function showSlides(n) {
       dots[i].className = dots[i].className.replace(" active", "");
     }
   slides[slideIndex-1].style.display = "block";
-  // dots[slideIndex-1].className += " active"; 
+  dots[slideIndex-1].className += " active"; 
 }
 //FAQ
 var acc = document.getElementsByClassName("accordion");
